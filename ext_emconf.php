4877a4c530ee8d9444847bf8c009cdd36b52f7cb
Georg Ringer <georg.ringer@gmail.com>
2023-12-20 13:47:56 +0100

    [TASK] Release 6.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => '',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '6.0.1',
+    'version' => '6.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.5.99',
-            'news' => '11.1.0-11.99.9',
+            'news' => '11.3.0-11.99.9',
         ],
         'conflicts' => [],
         'suggests' => [],
