d5d48fe3d5fb762dffceaf781b238a22ebe7d10e
Stefan <mail@createdwithlove.de>
2025-09-24 07:37:15 +0200

    Update tx_eventnews_domain_model_organizer.php

--- Configuration/TCA/tx_eventnews_domain_model_organizer.php
+++ Configuration/TCA/tx_eventnews_domain_model_organizer.php
@@ -106,9 +106,7 @@ $tx_eventnews_domain_model_organizer = [
             'label' => 'LLL:EXT:eventnews/Resources/Private/Language/locallang_db.xlf:tx_eventnews_domain_model_organizer.description',
             'config' => [
                 'type' => 'text',
-                'cols' => 30,
-                'rows' => 4,
-                'eval' => 'trim',
+                'enableRichtext' => true,
             ],
         ],
         'link' => [
