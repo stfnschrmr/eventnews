4163f19b615bc063bab6c778b7c4e76ff1cf9b6d
Stefan <mail@createdwithlove.de>
2025-09-24 07:36:37 +0200

    Update tx_eventnews_domain_model_location.php

--- Configuration/TCA/tx_eventnews_domain_model_location.php
+++ Configuration/TCA/tx_eventnews_domain_model_location.php
@@ -110,8 +110,7 @@ $tx_eventnews_domain_model_location = [
             'label' => 'LLL:EXT:eventnews/Resources/Private/Language/locallang_db.xlf:tx_eventnews_domain_model_location.description',
             'config' => [
                 'type' => 'text',
-                'cols' => 30,
-                'rows' => 4,
+                'enableRichtext' => true,
                 'eval' => 'trim',
             ],
         ],
